04ba46bfca15f529d0079b4c91fb14547509ce9b
Ahmed Mahmood <ahmed@retrocausal.com>
2024-10-29 12:24:14 +0000

    Edited video_to_frames.py so now it names the frames in a 4 digit format to avoid any file sorting issues

--- video_to_frames.py
+++ video_to_frames.py
@@ -4,22 +4,27 @@ import tqdm
 import os
 import glob
 
-if not os.path.exists('Data'):
-    os.mkdir("Data")
+# Create a dir to store all the video dirs(dir labelled the name of a video and contains frames from that video)
+if not os.path.exists('output'):
+    os.mkdir("output")
 
-path = sys.argv[1]
+# Define the path to the directory of videos
+path = '/workspace/ikea_full_data/train_vids/'
 
 for video in tqdm.tqdm(glob.glob(path + '*')):
     vid_name = video.split('/')[-1].split('.')[0]
     vidcap = cv2.VideoCapture(video)
     success,image = vidcap.read()
     count = 0
-    if not os.path.exists(f'Data/{vid_name}'):
-        os.mkdir(f'Data/{vid_name}')
+    vid_dir = f'output/{vid_name}'
+    # Create a video dir for the current video
+    if not os.path.exists(vid_dir):
+        os.mkdir(vid_dir)
+    # If the video dir for this video already exists then continue on to the next video
     else:
         continue
+    # If we just made the video dir, then fill it with the frames from the vid
     while success:
-        cv2.imwrite(f"Data/{vid_name}/frame%d.jpg" % count, image)        
+        cv2.imwrite(f"{vid_dir}/{count:04d}.jpg", image)
         success,image = vidcap.read()
         count += 1
-        
